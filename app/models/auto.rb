b28717d757d7cd675a4ce053756da610080d64a7
GrandUncleHank <tarosngh@gmail.com>
2017-10-06 18:29:00 +0000

    Created validations for autos

--- app/models/auto.rb
+++ app/models/auto.rb
@@ -1,3 +1,8 @@
 class Auto < ActiveRecord::Base
     belongs_to :user
+    validates :title, presence: true, length: {minimum: 3, maximum: 50}
+    validates :price, presence: true, length: {minimum: 1, maximum: 8}
+    validates :description, presence: true, length: {minimum: 10, maximum: 300}
+    validates :contact, presence: true, length: {minimum: 3, maximum: 50}
+    
 end
